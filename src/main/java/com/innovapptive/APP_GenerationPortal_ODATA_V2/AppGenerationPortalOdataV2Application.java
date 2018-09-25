b20e2b38b855008a13332519b37311fb88c7d41b
E5000270 <E5000270@IGDCLT0102.internal.innovapptive.com>
2018-09-25 18:25:17 +0530

    code commited

---
+++ src/main/java/com/innovapptive/APP_GenerationPortal_ODATA_V2/AppGenerationPortalOdataV2Application.java
@@ -0,0 +1,12 @@
+package com.innovapptive.APP_GenerationPortal_ODATA_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGenerationPortalOdataV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGenerationPortalOdataV2Application.class, args);
+	}
+}
