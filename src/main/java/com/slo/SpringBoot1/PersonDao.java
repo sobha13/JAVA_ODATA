624b87f27a2b99e14f4610292f23f3353415cdc3
E5000270 <E5000270@IGDCLT0102.internal.innovapptive.com>
2018-09-26 11:45:49 +0530

    spring boot code commited

---
+++ src/main/java/com/slo/SpringBoot1/PersonDao.java
@@ -0,0 +1,9 @@
+package com.slo.SpringBoot1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+    @Repository
+	public interface PersonDao extends JpaRepository<PersonPojo, Integer>{
+
+}
---
+++ src/main/java/com/slo/SpringBoot1/PersonPojo.java
@@ -0,0 +1,68 @@
+package com.slo.SpringBoot1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employeenew")
+public class PersonPojo {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String dob;
+	private String doj;
+	private String nationality;
+	
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Column(name="firstName")
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	
+	
+}
